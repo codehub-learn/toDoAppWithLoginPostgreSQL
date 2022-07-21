1125e1344ef0126bebfd617aa310eec7becf4db7
Ioannis Daniil <jondan97@gmail.com>
2022-07-21 19:54:16 +0300

    made APP_ID final

--- src/main/java/gr/codehub/toDoAppWithLogin/controller/RestController.java
+++ src/main/java/gr/codehub/toDoAppWithLogin/controller/RestController.java
@@ -8,7 +8,7 @@ import java.util.concurrent.ThreadLocalRandom;
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
-    private static int APP_ID = ThreadLocalRandom.current().nextInt();
+    private static final int APP_ID = ThreadLocalRandom.current().nextInt();
 
     @GetMapping("/id")
     public int getID() {
